cea4098a70583d1e1e6a8ef2974ca649f3487981
Donnell A Rawlings <donnierawlings@gmail.com>
2012-09-23 00:46:34 -0400

    can this work

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 TheBrands::Application.initialize!
 
-Time::DATE_FORMATS[:due_time] = "%d %B  %Y" 
+#Time::DATE_FORMATS[:due_time] = "%d %B  %Y" 
