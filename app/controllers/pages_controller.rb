f924db67aa0279e6bdcb80ac3f2d06d014d36327
Donnell A Rawlings <donnierawlings@gmail.com>
2013-03-26 05:27:35 -0400

    now we are good

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	
 	def home
 	@uploads_slideshow = Upload.order("RANDOM()").where("private = ?", false).limit(4)
-	@uploads_rest = Upload.order("created_at DESC").where("private = ?", false).limit(12)
+	@uploads_rest = Upload.order("created_at DESC").where("private = ?", false).limit(9)
 	@videos_home = FunnyVideo.order("created_at DESC").offset(3).limit(9)
 	@videos_home2 = FunnyVideo.order("created_at DESC").limit(3)
 	@uploads_likes = Upload.plusminus_tally.limit(3)
