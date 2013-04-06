1e228601e59cb6616b8680c5b3ad7ca20f27718f
Donnell A Rawlings <donnierawlings@gmail.com>
2013-04-06 01:59:40 -0400

    not yet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def toggle_currentuser(uploads)
 
 		if current_user	
-			link_to image_tag("spritethumbs.png", :class => "tolike"), likeit_upload_path(uploads), :remote => true, :method => :post, :id => "to_like"
+			link_to image_tag("spritethumbs.png", :class => "tolike"), likeit_upload_url(uploads), :remote => true, :method => :post, :id => "to_like"
 		else
 			link_to image_tag("spritethumbs.png", :class => "tolike"), "#", :id => "joinus" 
 
@@ -11,7 +11,7 @@ module ApplicationHelper
 	def toggle_currentuserhate(uploads)
 
 		if current_user	
-			link_to image_tag("spritethumbs.png", :class => "tolike"),  hate_upload_path(uploads), :remote => true, :method => :post, :id => "ajax_trigger" 
+			link_to image_tag("spritethumbs.png", :class => "tolike"),  hate_upload_url(uploads), :remote => true, :method => :post, :id => "ajax_trigger" 
 		else
 			link_to image_tag("spritethumbs.png", :class => "tolike"), "#", :id => "joinus" 
 
