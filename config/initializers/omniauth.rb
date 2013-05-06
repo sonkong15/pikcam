74975d9b59471eeceddbc0673ab8c90a9e89f566
Donnell A Rawlings <donnierawlings@gmail.com>
2013-05-06 13:39:40 -0400

    come on now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :twitter, 'LKcWhFRlwwAMgwVExGVKAw', 'qNnMYgROAk0g1B45hEpnQjpsZoiTDgqQPsWBTKY'
   provider :facebook, '357983834258531', 'd12ea5fdf904da4b6a0ae016188a6931',
-  {:scope => 'publish_stream, offline_access, email', :image_size => "large", :authorize_params => { :display => 'popup'}, 
+  {:scope => 'publish_stream, offline_access, email', :image_size => "large",
    client_options: {ssl: {ca_file: Rails.root.join('app/assets/cacert.pem').to_s}}}  
 end
